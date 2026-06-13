1cde416fc815316b099ec6e2d75db99e144cd142
Gaurav06120714 <gauravganeshteegulla@gmail.com>
2026-06-13 18:44:29 +0530

    build: bump version to 5.0.0 for the V5 umbrella release

--- engine/include/vyro/core/Version.hpp
+++ engine/include/vyro/core/Version.hpp
@@ -5,11 +5,11 @@
 
 namespace vyro {
 
-inline constexpr u32 kVersionMajor = 4;
+inline constexpr u32 kVersionMajor = 5;
 inline constexpr u32 kVersionMinor = 0;
 inline constexpr u32 kVersionPatch = 0;
 
-inline constexpr const char* kVersionString = "4.0.0";
+inline constexpr const char* kVersionString = "5.0.0";
 inline constexpr const char* kEngineName    = "VyroEngine";
 
 } // namespace vyro
