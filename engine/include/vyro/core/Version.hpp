a84c1c8cf7bbabdbfa854076414ad898c9333ebc
Gaurav06120714 <gauravganeshteegulla@gmail.com>
2026-06-14 17:40:41 +0530

    build: bump version to 9.0.0 for the V9 umbrella release

--- engine/include/vyro/core/Version.hpp
+++ engine/include/vyro/core/Version.hpp
@@ -5,11 +5,11 @@
 
 namespace vyro {
 
-inline constexpr u32 kVersionMajor = 8;
+inline constexpr u32 kVersionMajor = 9;
 inline constexpr u32 kVersionMinor = 0;
 inline constexpr u32 kVersionPatch = 0;
 
-inline constexpr const char* kVersionString = "8.0.0";
+inline constexpr const char* kVersionString = "9.0.0";
 inline constexpr const char* kEngineName    = "VyroEngine";
 
 } // namespace vyro
